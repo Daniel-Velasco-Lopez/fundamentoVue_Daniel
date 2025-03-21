197f57fc8eb3740435b89748fdc5f4918d3f577e
Daniel Velasco López <danyinfo158@gmail.com>
2025-03-20 18:27:29 -0600

    Estructura de carpetas

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<script setup>
+//metodos
+</script>
+
+<template>
+  <!--Estructura html-->
+  <h1>Hola Mundo</h1>
+  <img class="imagen" src="https://st.depositphotos.com/1016440/2534/i/450/depositphotos_25344733-stock-photo-sunrise-at-the-beach.jpg" /><br>
+  <button class="btn">Hola soy un boton</button>
+  <br><br>
+  <p>Soy DANIEL VELASCO LOPEZ Y ESTA ES MI PRIMERA MODIFICACION</p>
+</template>
+
+<style scoped>
+  /*estilos css */
+  h1{
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 45px;
+    color: red;
+    text-shadow: 3px 3px 5px rgba(233, 227, 74, 0.3);
+  }
+  .btn{
+    border-radius: 15px;
+    background-color: #ffff;
+    color: black;
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  }
+  P{
+    font-family: cursive;
+    color: yellow;
+  }
+  .imagen{
+    width: 100%;
+    max-width: 500px;
+    border-radius: 10px;
+    box-shadow: 5px 5px 15px rgba(213, 97, 2, 0.3);
+    display: block;
+    margin: 20px auto;
+  }
+</style>
