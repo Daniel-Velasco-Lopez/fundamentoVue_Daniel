973c47ef84c1bc9f871cb90edf7011c891e54846
Daniel Velasco López <danyinfo158@gmail.com>
2025-03-21 15:14:40 -0600

    Cambios finales

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,7 +14,7 @@
 
                     </div>
                     <div class="card-body">
-
+                        <p class="card-text">Kawasaki Ninja H2R: La moto más rápida del mundo con su impresionante motor de 4 cilindros de 998 cc.</p>
                     </div>
                 </div>
             </div>
@@ -25,7 +25,7 @@
 
                     </div>
                     <div class="card-body">
-                        
+                        <p class="card-text">RAPTOR 700R SE - Yamaha Motos.</p>
                     </div>
                 </div>
             </div>
@@ -33,7 +33,39 @@
         <!--FIN DEL PRIMER ROW-->
 
         <!--INICIO DEL SEGUNDO ROW-->
-        
+        <div class="row mt-4">
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://clickpetroleoegas.com.br/wp-content/uploads/2024/05/Kawasaki-Ninja-H2R-1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Kawasaki Ninja H2R</h5>
+                        <p class="card-text">Kawasaki Ninja H2R: La moto más rápida del mundo con su impresionante motor de 4 cilindros de 998 cc.</p>
+                        
+                        <button class="btn btn-warning">Mas información</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://www.yamahamotos.cl/wp-content/uploads/2023/05/RAPTOR700RSE_NEGRO.jpg" class="card-img-top" alt="...">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">RAPTOR 700R SE</h5>
+                        <p class="card-text">RAPTOR 700R SE - Yamaha Motos.</p>                        
+                        <button class="btn btn-warning">Mas información</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card" style="width: 18rem;">
+                    <img src="https://ahorraseguros.mx/wp-content/uploads/2023/08/kawasaki-ninja-6366207_640-min.jpg" class="card-img-top" alt="...">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Motos Kawasaki</h5>
+                        <p class="card-text">La moto más rápida del mundo con su impresionante motor de 4 cilindros.</p>                        
+                        <button class="btn btn-warning">Mas información</button>
+                    </div>
+                </div>
+            </div>
+        </div>
         <!--FIN DEL SEGUNDO ROW-->
     </div>
 </template>
