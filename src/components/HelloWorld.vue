1e6a3f4671b56002f8aaf4831b5de3d0cc9f3e92
Daniel Velasco López <danyinfo158@gmail.com>
2025-03-21 13:48:20 -0600

    Primer Row

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,10 +3,39 @@
 </script>
 
 <template>
- <h1 class="text-center">REFACCIONARIA TIMÓN</h1>
- <di class="card"> 
+    <div class="container">
+        <h1 class="text-center">REFACCIONARIA TIMÓN</h1>
+        <!--INICIO DEL PRIMER ROW-->
+        <di class="row"> 
+            <div class="col-md-6">
+                <div class="card">
+                    <h6>Motocicletas</h6>
+                    <div class="card-header">
 
- </di>
+                    </div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <h6>Cuatrimotos</h6>
+                    <div class="card-header">
+
+                    </div>
+                    <div class="card-body">
+                        
+                    </div>
+                </div>
+            </div>
+        </di>
+        <!--FIN DEL PRIMER ROW-->
+
+        <!--INICIO DEL SEGUNDO ROW-->
+        
+        <!--FIN DEL SEGUNDO ROW-->
+    </div>
 </template>
 
 <style scoped>
