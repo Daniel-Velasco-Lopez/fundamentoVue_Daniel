a1c410ddb0a33e77f45a4cf81459a9c8a358ff12
Daniel Velasco López <danyinfo158@gmail.com>
2025-03-24 14:50:51 -0600

    Agregando informacion a CardPrincipal

---
+++ src/components/CardPrincipal.vue
@@ -0,0 +1,26 @@
+<template>
+    <di class="row"> 
+        <div class="col-md-6">
+            <div class="card">
+                <h6>Motocicletas</h6>
+                <div class="card-header">
+
+                </div>
+                <div class="card-body">
+                    <p class="card-text">Kawasaki Ninja H2R: La moto más rápida del mundo con su impresionante motor de 4 cilindros de 998 cc.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <h6>Cuatrimotos</h6>
+                <div class="card-header">
+
+                </div>
+                <div class="card-body">
+                    <p class="card-text">RAPTOR 700R SE - Yamaha Motos.</p>
+                </div>
+            </div>
+        </div>
+    </di>
+</template>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,30 +7,7 @@ import Card from './Card.vue';
     <div class="container">
         <h1 class="text-center">REFACCIONARIA TIMÓN</h1>
         <!--INICIO DEL PRIMER ROW-->
-        <di class="row"> 
-            <div class="col-md-6">
-                <div class="card">
-                    <h6>Motocicletas</h6>
-                    <div class="card-header">
-
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text">Kawasaki Ninja H2R: La moto más rápida del mundo con su impresionante motor de 4 cilindros de 998 cc.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="card">
-                    <h6>Cuatrimotos</h6>
-                    <div class="card-header">
-
-                    </div>
-                    <div class="card-body">
-                        <p class="card-text">RAPTOR 700R SE - Yamaha Motos.</p>
-                    </div>
-                </div>
-            </div>
-        </di>
+        
         <!--FIN DEL PRIMER ROW-->
 
         <!--Llamamos al componente importado-->
