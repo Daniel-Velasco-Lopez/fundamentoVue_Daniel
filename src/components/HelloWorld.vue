3fc0e23e85c9353af3fc8dc860d2597e5f1133df
Daniel Velasco López <danyinfo158@gmail.com>
2025-03-24 15:07:48 -0600

    Iportación y Llamado de CardPrincipal

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,7 @@
 <script setup>
+/// Importación del Card Principal
+import CardPrincipal from './CardPrincipal.vue';
+//Importación del 1er card
 import Card from './Card.vue';
 //metodos
 </script>
@@ -7,14 +10,12 @@ import Card from './Card.vue';
     <div class="container">
         <h1 class="text-center">REFACCIONARIA TIMÓN</h1>
         <!--INICIO DEL PRIMER ROW-->
-        
+        <card-principal/>
         <!--FIN DEL PRIMER ROW-->
 
         <!--Llamamos al componente importado-->
         <card/>
         <!--Fin del llamado al componente importado-->
-
-        <card/>
     </div>
 </template>
 
